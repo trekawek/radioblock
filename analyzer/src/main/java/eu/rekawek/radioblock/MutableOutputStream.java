fbe65e4e450c9fc321a61f8336925c3bedcb031e
Tomek Rekawek <rekawek@adobe.com>
2016-02-24 11:58:24 +0100

    Extracted MutableOutputStream.

---
+++ analyzer/src/main/java/eu/rekawek/radioblock/MutableOutputStream.java
@@ -0,0 +1,46 @@
+package eu.rekawek.radioblock;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public class MutableOutputStream extends OutputStream {
+
+    private final ByteBuffer buffer;
+
+    private final OutputStream os;
+
+    private volatile float volumeLevel = 1;
+
+    public MutableOutputStream(OutputStream os) {
+        this.buffer = ByteBuffer.allocate(4);
+        this.buffer.order(ByteOrder.LITTLE_ENDIAN);
+        this.os = os;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        buffer.put((byte) b);
+        if (buffer.position() == 4) {
+            float volumeLevelLocal = volumeLevel;
+
+            buffer.rewind();
+            short left = (short) (buffer.getShort() * volumeLevelLocal);
+            short right = (short) (buffer.getShort() * volumeLevelLocal);
+
+            buffer.clear();
+            buffer.putShort(left);
+            buffer.putShort(right);
+            buffer.rewind();
+            while (buffer.hasRemaining()) {
+                os.write(buffer.get());
+            }
+            buffer.clear();
+        }
+    }
+
+    public void setVolumeLevel(float volumeLevel) {
+        this.volumeLevel = volumeLevel;
+    }
+}
--- analyzer/src/main/java/eu/rekawek/radioblock/MuteStream.java
+++ analyzer/src/main/java/eu/rekawek/radioblock/MuteStream.java
@@ -4,10 +4,7 @@ import static java.util.Arrays.asList;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URISyntaxException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,45 +33,5 @@ public class MuteStream {
         });
         TeeInputStream tis = new TeeInputStream(System.in, mos);
         locator.analyse(tis);
-    }
-
-    public static class MutableOutputStream extends OutputStream {
-
-        private final ByteBuffer buffer;
-
-        private final OutputStream os;
-
-        private volatile float volumeLevel = 1;
-
-        public MutableOutputStream(OutputStream os) {
-            this.buffer = ByteBuffer.allocate(4);
-            this.buffer.order(ByteOrder.LITTLE_ENDIAN);
-            this.os = os;
-        }
-
-        @Override
-        public void write(int b) throws IOException {
-            buffer.put((byte) b);
-            if (buffer.position() == 4) {
-                float volumeLevelLocal = volumeLevel;
-
-                buffer.rewind();
-                short left = (short) (buffer.getShort() * volumeLevelLocal);
-                short right = (short) (buffer.getShort() * volumeLevelLocal);
-
-                buffer.clear();
-                buffer.putShort(left);
-                buffer.putShort(right);
-                buffer.rewind();
-                while (buffer.hasRemaining()) {
-                    os.write(buffer.get());
-                }
-                buffer.clear();
-            }
-        }
-
-        public void setVolumeLevel(float volumeLevel) {
-            this.volumeLevel = volumeLevel;
-        }
     }
 }

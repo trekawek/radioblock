e54fc4209c0fa7355caa9155bfbd7346a6572b8f
Tomek Rękawek <rekawek@adobe.com>
2018-06-24 10:21:51 +0200

    Use standard java command.

--- scripts/analyzer.sh
+++ scripts/analyzer.sh
@@ -3,11 +3,10 @@
 MUTE=10
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-
 rtl_fm -M wbfm -f 89.50M -g 0 \
- | /opt/jdk8/bin/java -jar analyzer-1.0.0-SNAPSHOT.jar 1 \
-   $DIR/commercial-start-32k-mono.raw 80 \
-   $DIR/commercial-end-32k-mono.raw 90 \
+ | java -jar analyzer-1.0.0-SNAPSHOT.jar 1 \
+   $DIR/commercial-start-32k-mono.raw 180 \
+   $DIR/commercial-end-32k-mono.raw 190 \
  | while read line
 do
   if [ "$line" == "Got jingle 0" ]; then

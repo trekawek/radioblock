5d22c10870b8018c465f6d1ac4a1de13ea0ce15f
Tomek Rękawek <rekawek@adobe.com>
2019-12-02 20:26:06 +0100

    Kill container if ices dies.

--- standalone/src/main/docker/ices/start.sh
+++ standalone/src/main/docker/ices/start.sh
@@ -6,4 +6,16 @@ envsubst < ices.xml.tmpl > ices.xml
 echo "Waiting for Icecast to initialize"
 sleep 10
 
+function watch_ices {
+  while true; do
+    sleep 5
+    if ! pgrep ices; then
+      pkill java
+      exit
+    fi
+  done
+}
+
+watch_ices &
+
 java -jar standalone.jar --cli 600 700 | exec ices ices.xml
